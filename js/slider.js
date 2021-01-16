3f55a006035ff03876f36cb58e046de2569b3c22
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-16 16:20:39 +0545

    Image Slider COmpelte !!

--- js/slider.js
+++ js/slider.js
@@ -18,17 +18,46 @@ class Carousel {
         this.sliderNextBtn = document.createElement('button');
         this.sliderPreviousBtn = document.createElement('button');
 
+        this.currentImageIndex = 0;
         this.images = this.slide.querySelectorAll('img');
         this.imageCount = this.images.length;
         this.imageWidth = (100 / this.imageCount).toFixed(6);
+        this.slideLeft = this.currentImageIndex * 100;
     }
 
-    render() {
-        this.addStyles();
+    animateToNthPicture = (n) => {
+        const targetWidth = n * 100;
+        const direction = this.currentImageIndex > n ? -1 : 1;
+        const timePerFrame = (this.transactionTime / ((n * 100) - this.slideLeft)) * direction;
+        this.currentImageIndex = n;
 
-        this.renderSliderButtons();
+        const animate = setInterval(() => {
+            this.slideLeft += direction;
+            this.slide.style.left = (-this.slideLeft) + '%';
+
+            if (direction === 1 ? this.slideLeft >= targetWidth : this.slideLeft <= targetWidth) {
+                clearInterval(animate)
+            }
+        }, timePerFrame);
     }
-    
+
+    showNextPicture = () => {
+        const nextImgIndex = this.currentImageIndex + 1;
+        if (nextImgIndex > this.imageCount - 1) this.animateToNthPicture(0);
+        else this.animateToNthPicture(nextImgIndex);
+    }
+
+    showPreviousPicture = () => {
+        const preImgIndex = this.currentImageIndex - 1;
+        if (preImgIndex < 0) this.animateToNthPicture(this.imageCount - 1);
+        else this.animateToNthPicture(preImgIndex - 1);
+    }
+
+    addListeners() {
+        this.sliderNextBtn.addEventListener("click", this.showNextPicture)
+        this.sliderPreviousBtn.addEventListener("click", this.showPreviousPicture)
+    }
+
     addStyles() {
         this.addSliderStyles();
         this.addButtonStyles();
@@ -45,13 +74,20 @@ class Carousel {
     }
 
     renderSliderButtons() {
-        this.sliderNextBtn.innerHTML = '&#10094;';
-        this.sliderPreviousBtn.innerHTML = '&#10095;';
+        this.sliderNextBtn.innerHTML = '&#10095;';
+        this.sliderPreviousBtn.innerHTML = '&#10094;';
 
         this.slider.appendChild(this.sliderNextBtn);
         this.slider.appendChild(this.sliderPreviousBtn);
+    }
+
+    render() {
+        this.addStyles();
+        this.addListeners();
+        this.renderSliderButtons();
     }
 }
 
 const carousel = new Carousel('first-slider');
 carousel.render();
+// carousel.animateToNthPicture(1)
